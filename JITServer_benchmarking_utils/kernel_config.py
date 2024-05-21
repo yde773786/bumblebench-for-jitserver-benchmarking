940c6bc51bb4f6891c993ea76958d22db786038f
Nikhil <yde773786@gmail.com>
2024-05-21 13:49:13 -0400

    Fixed kernel config bug

--- JITServer_benchmarking_utils/kernel_config.py
+++ JITServer_benchmarking_utils/kernel_config.py
@@ -7,16 +7,11 @@ def setup_kernel_args(json_file):
 
     for key in config.keys():
         if key == "threads":
+            jit_server_args.write('BumbleBench.classesToInvoc=')
             for thread in config[key]:
                 for kernel_config in thread["kernels"]:
                     jit_server_args.write(f'{kernel_config["kernel_name"]} {kernel_config["invoc_count"]} ')
                 jit_server_args.write(f'/ ')
             jit_server_args.write('\n')
-        elif key == "multi-threaded":
-            jit_server_args.write('BumbleBench.isMultiThreaded=')
-            if config[key]:
-                jit_server_args.write('true\n')
-            else:
-                jit_server_args.write('false\n')
 
     jit_server_args.close()

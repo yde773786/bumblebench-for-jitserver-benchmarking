bf0855a944ef120bae5b5539052c260da3afcbec
RichardUofTDesktop <richard.kha@mail.utoronto.ca>
2024-06-12 12:21:22 -0400

    Changes so the wrapper works with the round robin jitserver

--- JITServer_benchmarking_utils/runwrapper.py
+++ JITServer_benchmarking_utils/runwrapper.py
@@ -73,14 +73,14 @@ if __name__ == "__main__":
 
     # Run the normal server and the changed server in parallel
     # Each iteration has a warmup of the JITServer and then the actual benchmarking
-
+    os.environ['IsRandomJitServer'] = 'false'
     get_dir = ''
     for i in range(int(num_runs)):
         num = random.randint(0, 10000000)
         os.environ['TR_Seed'] = str(num)
 
         print(f"Normal JITServer run {i}")
-        os.environ['IsRandomJitServer'] = 'false'
+        os.environ['IsRoundRobinJitServer'] = 'false'
         cmd = f'{server_path} -XX:+JITServerLogConnections -XX:+JITServerMetrics -Xjit:verbose={{JITServer}},highActiveThreadThreshold=1000000000,veryHighActiveThreadThreshold=1000000000 -XcompilationThreads1'
         print("server command: " + cmd)
         proc = wait_for_server(cmd)
@@ -91,7 +91,7 @@ if __name__ == "__main__":
 
         os.environ['TR_Seed'] = str(num)
         print(f"Changed JITServer run {i}")
-        os.environ['IsRandomJitServer'] = 'true'
+        os.environ['IsRoundRobinJitServer'] = 'true'
         cmd = f'{server_path} -XX:+JITServerLogConnections -XX:+JITServerMetrics -Xjit:verbose={{JITServer}},highActiveThreadThreshold=1000000000,veryHighActiveThreadThreshold=1000000000 -XcompilationThreads1'
         print("server command: " + cmd)
         proc = wait_for_server(cmd)

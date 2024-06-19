f8fab92f0cfa2719943376d960e9c337c12be088
kRichard32 <richard.kha@mail.utoronto.ca>
2024-06-19 13:58:40 -0400

    changes to cdf grapher to support the continuous load wrapper:

--- JITServer_benchmarking_utils/cdf_grapher.py
+++ JITServer_benchmarking_utils/cdf_grapher.py
@@ -12,39 +12,65 @@ if __name__ == "__main__":
     )
 
     parser.add_argument('-d', '--data', required=True)
+    parser.add_argument('-clw', '--continuous_load_wrapper', action='store_true')
     args = vars(parser.parse_args())
     total_data = args['data']
+    continuous_load_wrapper = args['continuous_load_wrapper']
     total_data = total_data.split(",")
 
     data_frames = []
-    for data in total_data:
-        run = 0
-        data_1 = []
-        data_2 = []
 
-        normal_data = []
-        random_data = []
-        file_name = data.split('/')[-1]
-        file_name = file_name.replace(".csv","")
+    if continuous_load_wrapper:
+        for data in total_data:
+            run = 0
+            data_1 = []
+            data_2 = []
 
-        with open(data, "r") as file:
-            reader = csv.reader(file)
-            next(reader, None)
-            for row in reader:
-                if int(row[0]) != run:
-                    run = int(row[0])
-                    if data_1 != []:
-                        # normal_data.append(sum(data_1)/len(data_1))
-                        # random_data.append(sum(data_2)/len(data_2))
-                        normal_data.append(max(data_1))
-                        random_data.append(max(data_2))
-                        data_1 = []
-                        data_2 = []
-                data_1.append(float(row[2]))
-                data_2.append(float(row[3]))
-        df = pd.DataFrame({f'{file_name}_normal_server:': normal_data})
-        df2 = pd.DataFrame({f'{file_name}_altered_server:': random_data})
-        data_frames.extend([df, df2])
+            normal_data = []
+            random_data = []
+            file_name = data.split('/')[-1]
+            file_name = file_name.replace(".csv","")
+
+            with open(data, "r") as file:
+                reader = csv.reader(file)
+                next(reader, None)
+                for row in reader:
+                    if row[0] == "Normal":
+                        normal_data.append(float(row[2]))
+                    else:
+                        random_data.append(float(row[2]))
+            df = pd.DataFrame({f'{file_name}_normal_server:': normal_data})
+            df2 = pd.DataFrame({f'{file_name}_altered_server:': random_data})
+            data_frames.extend([df, df2])
+    else:
+        for data in total_data:
+            run = 0
+            data_1 = []
+            data_2 = []
+
+            normal_data = []
+            random_data = []
+            file_name = data.split('/')[-1]
+            file_name = file_name.replace(".csv","")
+
+            with open(data, "r") as file:
+                reader = csv.reader(file)
+                next(reader, None)
+                for row in reader:
+                    if int(row[0]) != run:
+                        run = int(row[0])
+                        if data_1 != []:
+                            # normal_data.append(sum(data_1)/len(data_1))
+                            # random_data.append(sum(data_2)/len(data_2))
+                            normal_data.append(max(data_1))
+                            random_data.append(max(data_2))
+                            data_1 = []
+                            data_2 = []
+                    data_1.append(float(row[2]))
+                    data_2.append(float(row[3]))
+            df = pd.DataFrame({f'{file_name}_normal_server:': normal_data})
+            df2 = pd.DataFrame({f'{file_name}_altered_server:': random_data})
+            data_frames.extend([df, df2])
 
     both = pd.concat(data_frames, axis=1)
     print(both)

19a76c69049f9d184ca71f0faf2487c18e8734d8
helloween <yde773786@gmail.com>
2024-06-20 13:50:02 -0400

    automate load

---
+++ JITServer_benchmarking_utils/automate_sweep.py
@@ -0,0 +1,10 @@
+import os
+
+STAGGER = [4, 6, 8, 10, 12]
+
+for stagger in STAGGER:
+    os.system('python3 continuous_load_wrapper.py  -o ~/openj9-openjdk-jdk17/build/linux-x86_64-server-release/jdk/bin -c compiler_config.json -k kernel_config.json -b .. -n 10 -s {stagger} -t 600')
+
+    for directory in os.listdir("JITServer_benchmarking_utils"):
+        if os.path.isdir(directory) and "__pycache__" not in directory:
+            os.system(f"mv {directory} cw_{stagger}")

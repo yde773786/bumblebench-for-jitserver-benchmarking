7fc04d6b6a43f4205e68bdfc00415b5813a3390a
kRichard32 <richard.kha@mail.utoronto.ca>
2024-06-26 00:26:14 -0400

    adding additional support for gantt chart:

--- JITServer_benchmarking_utils/gantt_chart.py
+++ JITServer_benchmarking_utils/gantt_chart.py
@@ -1,4 +1,6 @@
 import argparse
+import random
+
 import pandas as pd
 import plotly.figure_factory as ff
 if __name__ == "__main__":
@@ -13,6 +15,7 @@ if __name__ == "__main__":
 
     data = []
     start_time = 0
+    client_set = set()
     with open(total_data, "r") as file:
         for row in file:
             if "#INFO:  Elapsed Time processing entry from client" in row:
@@ -20,10 +23,16 @@ if __name__ == "__main__":
                 client_id = splitted[1].split(">")[0]
                 end_time = float(splitted[2].split(">")[0])
                 bar = dict(Task=f'{client_id}', Start=start_time, Finish=start_time+end_time, Resource=f'{client_id}')
+                if client_id not in client_set:
+                    client_set.add(client_id)
                 start_time += end_time
                 data.append(bar)
+    r = lambda: random.randint(0,255)
+    colors = ['#%02X%02X%02X' % (r(),r(),r())]
+    for i in range(1, len(client_set)):
+        colors.append('#%02X%02X%02X' % (r(),r(),r()))
 
     df = pd.DataFrame(data)
-    fig = ff.create_gantt(df, index_col='Resource',  bar_width=0.4, show_colorbar=True, group_tasks=True)
+    fig = ff.create_gantt(df, index_col='Resource',colors=colors, bar_width=0.4, show_colorbar=True, group_tasks=True)
     fig.update_layout(xaxis_type='linear', autosize=False, width=1600, height=800)
     fig.show()

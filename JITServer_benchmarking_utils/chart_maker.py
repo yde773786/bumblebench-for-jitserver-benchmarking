8f50ce3fe03a870bc975faf53cfc0173d93ce584
kRichard32 <richard.kha@mail.utoronto.ca>
2024-06-26 20:55:17 -0400

    changes to make the chart maker work

--- JITServer_benchmarking_utils/chart_maker.py
+++ JITServer_benchmarking_utils/chart_maker.py
@@ -1,12 +1,5 @@
 import argparse
-import os
-import datetime as Date
-import random
 import subprocess
-from pathlib import Path
-
-import config_comparer
-from jitserver_benchmarker import main_function
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -14,8 +7,8 @@ if __name__ == "__main__":
         description="A Script that takes in a configuration of kernels and uses BumbleBench for JITServer to benchmark"
     )
 
-    parser.add_argument('-d1', '--data_cdf_hist', required=True)
-    parser.add_argument('-d2', '--data_gantt', required=True)
+    parser.add_argument('-d', '--data_cdf_hist', required=True)
+    parser.add_argument('-v', '--vlog_gantt', required=True)
     parser.add_argument('-f', '--figure_export_name', required=False)
     parser.add_argument('-clw', '--continuous_load_wrapper', action='store_true')
 
@@ -23,6 +16,19 @@ if __name__ == "__main__":
     args = vars(parser.parse_args())
 
     data_cdf_hist = args['data_cdf_hist']
-    data_gantt = args['data_gantt']
+    vlog_gantt = args['vlog_gantt']
     figure_export_name = args['figure_export_name']
     continuous_load_wrapper = args['continuous_load_wrapper']
+
+    cmd = f'python3 cdf_grapher.py -d {data_cdf_hist} -f cdf_chart_maker'
+    if continuous_load_wrapper is True:
+        cmd = cmd + " -clw"
+    proc = subprocess.Popen(cmd, shell=True)
+
+    cmd = f'python3 cdf_grapher.py -d {data_cdf_hist} -f histogram_chart_maker -his'
+    if continuous_load_wrapper is True:
+        cmd = cmd + " -clw"
+    proc = subprocess.Popen(cmd, shell=True)
+
+    cmd = f'python3 gantt_chart.py -d {vlog_gantt}'
+    proc = subprocess.Popen(cmd, shell=True)

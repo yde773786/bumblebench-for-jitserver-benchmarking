ae0fcc9d329a473e578f11558915deb7cb9366c2
RichardUofTDesktop <richard.kha@mail.utoronto.ca>
2024-07-11 15:36:53 -0400

    updating constants to match openj9 update

--- JITServer_benchmarking_utils/constants.py
+++ JITServer_benchmarking_utils/constants.py
@@ -1,2 +1,2 @@
-run_env_vars = [None,'IsRoundRobinJitServer','IsLeastDoneFirstJitServer']
-directories = ['fcfs_server', 'round_robin_server', 'least_done_first_server']
+run_env_vars = [None,'IsRoundRobinJitServer','IsLeastDoneFirstJitServer','IsFCFSImprovedServer']
+directories = ['fcfs_server', 'round_robin_server', 'least_done_first_server', 'improved_fcfs_server']

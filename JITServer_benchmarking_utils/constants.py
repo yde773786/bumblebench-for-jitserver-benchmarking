05ad3b256fac7761e5581111d287c07f2c835746
RichardUofTDesktop <richard.kha@mail.utoronto.ca>
2024-07-18 12:27:38 -0400

    new constants for new queing algo

--- JITServer_benchmarking_utils/constants.py
+++ JITServer_benchmarking_utils/constants.py
@@ -1,2 +1,2 @@
-run_env_vars = [None,'IsRoundRobinJitServer','IsLeastDoneFirstJitServer','IsFCFSImprovedServer']
-directories = ['fcfs_server', 'round_robin_server', 'least_done_first_server', 'improved_fcfs_server']
+run_env_vars = [None,'IsRoundRobinJitServer','IsImprovedLeastDoneFirstJitServer','IsLeastDoneFirstJitServer','IsFCFSImprovedServer']
+directories = ['fcfs_server', 'round_robin_server','ildf_server', 'least_done_first_server', 'improved_fcfs_server']

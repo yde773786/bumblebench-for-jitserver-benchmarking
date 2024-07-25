4b438abd3b5dccffad3d7bcfaa80be5320639bc7
kRichard32 <richard.kha@mail.utoronto.ca>
2024-07-25 01:55:53 -0400

    trying to get error

--- JITServer_benchmarking_utils/continuous_load_wrapper.py
+++ JITServer_benchmarking_utils/continuous_load_wrapper.py
@@ -67,8 +67,6 @@ def wait_for_docker_server(command, container):
 def start_docker_server(cmd, queue, container):
     queue.empty()
     server_vlog_file = open("servervlog.txt", "wb")
-    print(f'{cmd} 2>/root/servererror.txt')
-    exit()
     stream = docker_tools.execute_container_commmand(container,f'{cmd} >>/root/servererror.txt')[1]
 
     while True:

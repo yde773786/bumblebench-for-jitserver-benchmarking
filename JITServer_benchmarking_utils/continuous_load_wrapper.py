8b34cae32375a84d1e7089b6d6c6db485d5652f6
kRichard32 <richard.kha@mail.utoronto.ca>
2024-07-24 23:07:40 -0400

    small fix

--- JITServer_benchmarking_utils/continuous_load_wrapper.py
+++ JITServer_benchmarking_utils/continuous_load_wrapper.py
@@ -57,7 +57,7 @@ def wait_for_docker_server(command, container):
 
     while True:
         line = queue.get()
-        print(line)
+        print(line.decode("utf-8"))
         if line == "JITServer is ready to accept incoming requests":
             return docker_server
 

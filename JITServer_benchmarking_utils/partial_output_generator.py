d03344abefaba95b56f09a156d982da9bda28519
kRichard32 <richard.kha@mail.utoronto.ca>
2024-08-02 00:37:47 -0400

    fixing bug with partial_output_generator

--- JITServer_benchmarking_utils/partial_output_generator.py
+++ JITServer_benchmarking_utils/partial_output_generator.py
@@ -11,6 +11,7 @@ def generate_report_renaissance(directories, per_client_report_file, total_data,
             for key in inner_dict.keys():
                 results = inner_dict[key]
                 for j in range(len(results)):
+                    print(results)
                     time = round(float(results[j]["duration_ns"])/1000000000,2)
                     per_client_report_file.write(f"{directories[q]}, {i + 1}, {j + 1}, {time}\n")
 

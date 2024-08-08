ada8f4c74804d08959a340152a99d78292953935
Nikhil <yde773786@gmail.com>
2024-08-08 11:45:07 -0400

    Temp hist

---
+++ JITServer_benchmarking_utils/temperature_histogram.py
@@ -0,0 +1,16 @@
+import sys
+import matplotlib.pyplot as plt
+
+INPUT = open(sys.argv[1], 'r')
+
+temperature = {'cold': 0, 'warm': 0, 'hot': 0, 'scorching': 0}
+
+# read line by line
+for line in INPUT:
+    
+    if 'is compilation' in line:
+        spl = line.split()
+        temperature[spl[3][:-1]] += int(spl[4])
+
+plt.bar(temperature.keys(), temperature.values())
+plt.show()

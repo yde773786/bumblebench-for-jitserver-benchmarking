f937e6e9636d9867aa034f9744492fb4b0c48729
kRichard32 <richard.kha@mail.utoronto.ca>
2024-08-31 19:54:53 -0400

    organizing code

--- JITServer_benchmarking_utils/constants.py
+++ JITServer_benchmarking_utils/constants.py
@@ -2,9 +2,9 @@ run_env_vars = [None,'IsRoundRobinJitServer','IsImprovedLeastDoneFirstJitServer'
 directories = ['fcfs_server', 'round_robin_server','ildf_server', 'least_done_first_server', 'improved_fcfs_server', 'random_server']
 font_size = 9
 legend_size = 8
-COLUMN_HEIGHT = 2
 COLUMN_WIDTH = 241.14749 / 72.27
-# CDF (8,4) 16
-# TEMP (4,4) 14
-# VS (8,4) 16
-# DIST (8,4) 16
+COLUMN_HEIGHT = 2
+# 9,8 CDF (COLUMN_WIDTH,2)
+# 9,8 TEMP (COLUMN_WIDTH,2)
+# 9,8 VS (COLUMN_WIDTH,2)
+# 9,8 DIST (COLUMN_WIDTH,2)
